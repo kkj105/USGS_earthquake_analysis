05eb418af67aee05af6e6ebd818a99c7c56ea3db
Kirstie Jones <kirstiekjones@gmail.com>
2020-10-20 12:12:53 -0700

    progress

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,64 +15,56 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 // read in data
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
-var geojson;
 
-d3.json(link, function(data) {
-    console.log(data)
+d3.json(link, function(response) {
+    console.log(response)
 
-    //create a choropleth layer
-    geojson = L.choropleth(data, {
+    function fillColor(feature) {
+        var quakeDepth = feature.geometry.coordinates[2]
+        switch (true) {
+            case quakeDepth <= 10: return "#008000";
+            case quakeDepth <=30: return "#00F00";
+            case quakeDepth <=50 : return "#FFFF00";
+            case quakeDepth <=70: return "#FFB347";
+            case quakeDepth <90: return "#CC5500";
+            case quakeDepth >=90: return "#FF0000" 
+        };
+    }
 
-        // Define what property in the features to use
-        valueProperty: "properties",
-
-        // set color scale
-        scale: ["#ffff00", "#00FFFF"],
-
-        // number of breaks in the step range
-        steps: 10,
-
-        mode: "q", 
-        style: {
-            color: "#fff",
-            weight: 1,
-            fillOpacity: 0.8
-        },
-
-        // Bind pop-up to each layer
-        onEachFeature: function(feature, layer) {
-            layer.bindPopUp("Magnitute: " + features.properties.mag + "<br>Earthquake Data:<br>" + 
-                "Depth" + features.geometry.coordinates[3]);
-        }
-    }).addTo(myMap);
-
-
-
-    // Set up legend
-    var legend = L.control({ position: "topright"});
-    legend.onAdd = function() {
-        var div = L.DomUtil.create("div", "info legend");
-        var limits = geojson.options.limits;
-        var colors = geojson.options.colors;
-        var labels = [];
-        
-        limits.forEach(function(limit, index) {
-            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-        });
-
-        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-        return div;
-    };
-
-    // Add legend to map
-    legend.addTo(myMap);
-    
-    // for (var i = 0; i < response.length; i++) {
-    //     var location = response[i].location;
-
-    //     if (location) {
-    //         quakeArray.push([location.features.geometry.coordinates[1], location.features.geometry.coordinates[0]]).addTo(myMap)
+    // function radius(feature) {
+    //     var quakeRadius = feature.properties.mag
+    //     switch (true) {
+    //         case quakeRadius 
     //     }
-    // }
+    //}
 
-});
+    L.geoJSON(link, {
+        style: function(feature) {
+            return {
+                 opacity: 1,
+                fillColor: fillColor(feature)
+            }
+
+        }
+    })
+})
+
+
+    // // Set up legend
+    // var legend = L.control({ position: "topright"});
+    // legend.onAdd = function() {
+    //     var div = L.DomUtil.create("div", "info legend");
+    //     var limits = geojson.options.limits;
+    //     var colors = geojson.options.colors;
+    //     var labels = [];
+        
+    //     limits.forEach(function(limit, index) {
+    //         labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    //     });
+
+    //     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    //     return div;
+    // };
+
+    // // Add legend to map
+    // legend.addTo(myMap);
