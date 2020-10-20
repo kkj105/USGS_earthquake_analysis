eb29a89047ff75e8d8edc7994fbcfd4126876d26
Kirstie Jones <kirstiekjones@gmail.com>
2020-10-19 20:20:46 -0700

    work progress

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,7 +15,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 // read in data
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
-
+var geojson;
 
 d3.json(link, function(data) {
     console.log(data)
@@ -24,15 +24,55 @@ d3.json(link, function(data) {
     geojson = L.choropleth(data, {
 
         // Define what property in the features to use
-        valueProperty: "properties"
-    })
+        valueProperty: "properties",
 
-    for (var i = 0; i < response.length; i++) {
-        var location = response[i].location;
+        // set color scale
+        scale: ["#ffff00", "#00FFFF"],
 
-        if (location) {
-            quakeArray.push([location.features.geometry.coordinates[1], location.features.geometry.coordinates[0]]).addTo(myMap)
+        // number of breaks in the step range
+        steps: 10,
+
+        mode: "q", 
+        style: {
+            color: "#fff",
+            weight: 1,
+            fillOpacity: 0.8
+        },
+
+        // Bind pop-up to each layer
+        onEachFeature: function(feature, layer) {
+            layer.bindPopUp("Magnitute: " + features.properties.mag + "<br>Earthquake Data:<br>" + 
+                "Depth" + features.geometry.coordinates[3]);
         }
-    }
+    }).addTo(myMap);
 
-})
+
+
+    // Set up legend
+    var legend = L.control({ position: "topright"});
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+        var limits = geojson.options.limits;
+        var colors = geojson.options.colors;
+        var labels = [];
+        
+        limits.forEach(function(limit, index) {
+            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+        });
+
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+
+    // Add legend to map
+    legend.addTo(myMap);
+    
+    // for (var i = 0; i < response.length; i++) {
+    //     var location = response[i].location;
+
+    //     if (location) {
+    //         quakeArray.push([location.features.geometry.coordinates[1], location.features.geometry.coordinates[0]]).addTo(myMap)
+    //     }
+    // }
+
+});
