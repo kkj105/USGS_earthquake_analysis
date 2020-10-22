b31f28c22f5631abf7d46fac8dbc0b3dfc7f2af4
Kirstie Jones <kirstiekjones@gmail.com>
2020-10-22 13:57:42 -0700

    still stuck

--- static/js/logic.js
+++ static/js/logic.js
@@ -82,6 +82,14 @@ d3.json(link, function(response) {
         var depths = ["-10-10", "10-30", "30-50", "50-70", "70-90"," 90+"];
         var labels = [];
 
+        function getColor(depths) {
+            return depths >= 90 ? "#008000":
+                depths >=70 ? "#FF4500":
+                depths >= 50 ? "#FFA500":
+                depths >= 30 ? "#ffff00":
+                depths >= 10 ? "#b4cd32":
+                             "#73cd32";
+        }
         // Populate the legend
         for (var i = 0; i ,depths.length; i++) {
             div.innerHTML +=
@@ -90,9 +98,6 @@ d3.json(link, function(response) {
         }
         return div;
     };
-
-    
-
     // Add legend to map
     legend.addTo(myMap);
 
